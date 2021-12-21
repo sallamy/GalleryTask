cb362da86940a980fc689d8d83ad5c81472cd83c
sala7 <mohamed.sallam@contactcars.com>
2021-12-22 01:21:28 +0200

    Build Mvvm Archi-Files

---
+++ VOISTask/VOISTask/View/PhotoDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoDetailsViewController.swift
+//  VOISTask
+//
+//  Created by concarsadmin on 12/22/21.
+//
+
+import UIKit
+
+class PhotoDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
