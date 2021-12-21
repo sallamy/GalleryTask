e4f2381ae686640340b3eb0db9540caf4d8c4756
sala7 <mohamed.sallam@contactcars.com>
2021-12-22 01:40:40 +0200

    add Integration Api

--- VOISTask/VOISTask/Model/Photo.swift
+++ VOISTask/VOISTask/Model/Photo.swift
@@ -2,7 +2,31 @@
 //  Photo.swift
 //  VOISTask
 //
-//  Created by concarsadmin on 12/22/21.
+//  Created by sala7 on 12/22/21.
 //
 
 import Foundation
+
+protocol PhotoViewData {
+    var authorName: String { get }
+    var photoURL: String { get }
+}
+
+struct Photo: Codable {
+    private let id: String?
+    private let author: String?
+    private let width: Int?
+    private let height: Int?
+    private let url: String?
+    private let download_url: String?
+}
+
+extension Photo: PhotoViewData {
+    var authorName: String {
+        return author ?? ""
+    }
+    
+    var photoURL: String {
+        return url ?? ""
+    }
+}
